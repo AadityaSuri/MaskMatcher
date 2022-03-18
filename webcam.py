13f4b753a531d99f384d496b5bb299a3f5d9c776
Aaditya Suri <84222486+AadityaSuri@users.noreply.github.com>
2022-03-18 10:19:56 -0700

    added comments

--- webcam.py
+++ webcam.py
@@ -1,35 +1,37 @@
-import cv2
-import sys
+# citation: https://github.com/shantnu/Webcam-Face-Detect
+#           https://github.com/opencv/opencv/tree/master/data/haarcascades
 
-cascPath = "haarcascade_frontalface_default.xml"
+import cv2
+
+cascPath = "haarcascade_eye.xml"
 faceCascade = cv2.CascadeClassifier(cascPath)
 
-video_capture = cv2.VideoCapture(0)
+video_capture = cv2.VideoCapture(0) # live webcam feed object
 
 while True:
-    # Capture frame-by-frame
     ret, frame = video_capture.read()
-
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    # converts image to grayscale
     faces = faceCascade.detectMultiScale(
         gray,
         scaleFactor=1.1,
         minNeighbors=5,
-        minSize=(30, 30),
-        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+        minSize=(30, 30)
     )
 
-    # Draw a rectangle around the faces
+    #draw rectangles around detected object
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-    # Display the resulting frame
+    #show webcam stream
     cv2.imshow('Video', frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    #exit condition
+    if cv2.waitKey(1) & 0xFF == ord('E'):
         break
 
-# When everything is done, release the capture
+    cv2.imshow('Video', frame)
 video_capture.release()
 cv2.destroyAllWindows()
+
