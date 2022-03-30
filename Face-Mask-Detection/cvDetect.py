d23be8cf355e337beabb24ca593944936953dcd4
Aaditya Suri <84222486+AadityaSuri@users.noreply.github.com>
2022-03-30 08:42:29 -0700

    cvDetect with eye detection

--- Face-Mask-Detection/cvDetect.py
+++ Face-Mask-Detection/cvDetect.py
@@ -4,64 +4,42 @@ from main import CNN
 import torch
 from torchvision import transforms
 from PIL import Image
-from cvCrop import cropFace
+import torch.nn.functional as F
+# from cvCrop import cropFace
 
-# returns 0 if mask is detected, 1 if not detected, -1 if person is not detected
 
 def isWearingMask(image_path):
-    print("started")
-    transform_3 = transforms.Compose(
-        [transforms.Resize((128, 128)),
+    transform = transforms.Compose(
+        [transforms.Resize((32, 32)),
          transforms.ToTensor(),
          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
     )
 
-    # image_path = "https___cdn.cnn.com_cnnnext_dam_assets_201107093022-02-centered-main-bar-final-biden.jpg"  # add original image path here
-
-    isFace = cropFace(image_path)
-
     classes = ("with_mask", "without_mask")
 
+    img = cv2.imread(image_path)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    # img = cv2.imread(image_path_new)
-    #
-    # img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    #
-    # face_data = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
-    #
-    # found = face_data.detectMultiScale(img_gray,
-    #                                    minSize=(20, 20))
-    # amount_found = len(found)
+    eye_data = cv2.CascadeClassifier("haarcascade_eye.xml")
+    found = eye_data.detectMultiScale(img_gray, 1.1, 4)
 
-    if isFace != 0:
-        image_path_new = "face.jpg"
+    amount_found = len(found)
 
+    if amount_found != 0:
         model = CNN()
         model.load_state_dict(torch.load("model.pth"))
 
         model.eval()
 
-        img = Image.open(image_path_new)
-        x = transform_3(img)
+        img = Image.open(image_path).convert('RGB')
+        x = transform(img)
         x = x.unsqueeze(0)
 
-        output = model(x)  # Forward pass
-        pred = torch.argmax(output, 1)
-        # print('Image predicted as', classes[pred])
-        #
-        # plt.subplot(1, 1, 1)
-        # plt.imshow(img)
-        # plt.show()
-
-        return int(pred)
+        output = model(x)
+        pred = F.softmax(output, dim=1)
+        return pred
     else:
-        # print("no face detected")
-        # plt.subplot(1, 1, 1)
-        # plt.imshow(Image.open(image_path))
-        # plt.show()
-
         return -1
 
-print(isWearingMask("webcamImage.png"))
-
+# print(isWearingMask('bg.png'))
